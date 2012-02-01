d633e82ccbfb4f1987a3e96c997fc87a9eafe65e
Ervin Sukardi <emsukardi@gmail.com>
2012-01-31 21:35:59 -0800

    Added tests. Is there a problem with the |? Run to check

--- lab1/2script.sh
+++ lab1/2script.sh
@@ -2,16 +2,31 @@
 :   semicolon test
      : spaces should not matter
 : : :
-		 
-#simple command tests
+
+# Simple command tests
 echo standard echo
 (echo echo in shell)
 
-# redirection tests
+# Output Redirection
 echo this is my first test file > testfiles
 cat testfiles
 echo overwrite file > testfiles
-cat < testfiles
-cat < FILEDOESNOTEXIST  #should produce an error
-rm testfiles
 
+# Input Redirection
+cat < testfiles
+cat < FILEDOESNOTEXIST  # should produce an error in both cases
+
+# Simultaneous Output and Input Redirection
+sort < testfiles > testSorted
+
+# Nonalpha character tests
+expr 1 + 3 - 4 / 2 
+
+# Pipe tests
+echo hello world | sort
+ls -a | wc  -l
+who | sort > testfiles
+
+# cleanup
+rm testfiles
+rm testSorted
